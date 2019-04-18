9adda10796532dc2ccde88c2354b3f3729746f2a
jura <jura.webdeveloper@gmail.com>
2019-04-17 22:23:46 -0300

    print print_r var_dump

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 	echo 'Hello world!' . '<br>' . PHP_EOL;
 	echo 'Hello world 2!' . '<br>' . PHP_EOL;
+	print 'ABC' . '<br>';
+	$vetor = array('Palio','Gol','Fiesta','Corsa');
+	var_dump($vetor);
+	print '<br>';
+	print_r($vetor);
 ?>
 <!DOCTYPE html>
 <html>
