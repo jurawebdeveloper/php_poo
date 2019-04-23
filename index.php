2328936fd4b2da82baa5b97526eef20b5c690ccf
jura <jura.webdeveloper@gmail.com>
2019-04-22 21:48:37 -0300

    switch

--- index.php
+++ index.php
@@ -55,6 +55,24 @@
 	
 	echo $a;
 	
+	print '<br>';
+	
+	$i = 1;
+	switch ($i) {
+		case 0:
+			print 'i é igual a zero.';
+			break;
+		case 1:
+			print 'i é igual a um.';
+			break;
+		case 2:
+			print 'i é igual a dois.';
+			break;
+		default:
+			print 'i não é 0, nem 1 e nem 2';
+	}
+	print '<br>';
+	
 ?>
 <!DOCTYPE html>
 <html>
