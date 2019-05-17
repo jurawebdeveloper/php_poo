876b5c5c797f07cafa3d06154a952c9ad9f4947f
jura <jura.webdeveloper@gmail.com>
2019-05-16 21:54:02 -0300

    declaracao concatenacao

---
+++ c01/manipulandostr.php
@@ -0,0 +1,42 @@
+<?php
+	$fruta = 'maçã';
+	print "como $fruta";
+	
+	print '<br>';
+	/*
+	$texto = <<<CHAVE
+	linha1
+	nesta área
+	
+	CHAVE;
+	echo $texto;
+	*/ //não funcionou
+	print '<br>';
+	$fruta = 'maçã';
+	echo $fruta . ' é a fruta de Adão' . PHP_EOL;
+	print '<br>';
+	echo "{$fruta} é a fruta de Adão" . PHP_EOL;
+	
+	
+	print '<br>';
+	$a = 1234;
+	echo "O salário é $a";
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+	print '<br>';
+?>
