5f1a271ff1e37019cd8c20b1afb669235d24eab4
jura <jura.webdeveloper@gmail.com>
2019-05-18 15:46:57 -0300

    cap2 produto ajuste prints

--- c02/classes.php
+++ c02/classes.php
@@ -2,14 +2,16 @@
 include 'classes/Produto.php';
 
 $p1 = new Produto;
-$p1->descricao = 'Chocolate';
-$p1->estoque = 10;
-$p1->preco = 7;
+$p1->setDescricao('Chocolate');
+$p1->setEstoque(10);
+$p1->setPreco(7);
 
 $p2 = new Produto;
-$p2->descricao = 'Café';
-$p2->estoque = 20;
-$p2->preco = 4;
+$p1->setDescricao('Cafe');
+$p1->setEstoque(20);
+$p1->setPreco(4);
+
+
 
 var_dump($p1);
 print '<br>';
@@ -18,14 +20,14 @@ var_dump($p2);
 print '<br>';
 print '<br>';
 
-print "O estoque de {$p1->descricao} é {$p1->estoque}<br>";
-print "O preco de {$p1->descricao} é {$p1->preco}<br>";
+print "O estoque de {$p1->getDescricao()} é {$p1->getEstoque()}<br>";
+print "O preco de {$p1->getDescricao()} é {$p1->getPreco()}<br>";
 
 $p1->aumentarEstoque(11);
 $p1->reajustarPreco(20);
 
-print "O estoque de {$p1->descricao} é {$p1->estoque}<br>";
-print "O preco de {$p1->descricao} é {$p1->preco}<br>";
+print "O estoque de {$p1->getDescricao()} é {$p1->getEstoque()}<br>";
+print "O preco de {$p1->getPreco()} é {$p1->getPreco()}<br>";
 
 
 ?>
