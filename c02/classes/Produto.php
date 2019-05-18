a7ce8de527c2adc4f8cdfc44d4b902ed2ea1e696
jura <jura.webdeveloper@gmail.com>
2019-05-18 15:38:51 -0300

    cap2 produto getter and setters

--- c02/classes/Produto.php
+++ c02/classes/Produto.php
@@ -4,6 +4,46 @@ class Produto {
 	private $estoque;
 	private $preco;
 	
+	public function setDescricao($descricao) {
+		if (is_string($descricao)) {
+			$this->descricao = $descricao;
+		}
+	}	
+	public function getDescricao() {
+		return $this->descricao;
+	}
+	
+	
+	
+	
+	
+	public function setPreco($preco) {
+		if (is_numeric($preco) AND $preco > 0) {
+			$this->preco = $preco;
+		}
+	}
+	public function getPreco() {
+		return $this->preco;
+	}
+	
+	
+	
+	
+	
+	public function setEstoque($estoque) {
+		if (is_numeric($estoque) AND $estoque > 0) {
+			$this->estoque = $estoque;
+		}
+	}	
+	public function getEstoque() {
+		return $this->estoque;
+	}
+	
+	
+	
+	
+	
+	
 	public function aumentarEstoque($unidades) {
 		if(is_numeric($unidades) AND $unidades >= 0) {
 			$this->estoque += $unidades;
