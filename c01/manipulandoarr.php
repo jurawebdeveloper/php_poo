20d848fd9b85359c1b52ddee8edde064b8dddfa0
jura <jura.webdeveloper@gmail.com>
2019-05-17 21:10:13 -0300

    acessando, arr multidimensional, funcoes unshift e push

--- c01/manipulandoarr.php
+++ c01/manipulandoarr.php
@@ -27,10 +27,36 @@
 		echo "$chave => $fruta <br>";
 	}
 	print '<br>';
+
+	print_r($frutas);
 	print '<br>';
+
+	$carros = array('Palio' => array('cor'=>'azul',
+									 'potencia'=>'1.0',
+									 'opcionais'=>'Ar cond.'),
+					'Corsa' => array('cor'=>'cinza',
+									 'potencia'=>'1.6',
+									 'opcionais'=>'mp3'),
+
+					'Gol' => array('cor'=>'branca',
+									 'potencia'=>'1.6',
+									 'opcionais'=>'metálica')
+
+	);
+	echo $carros['Palio']['opcionais'];
 	print '<br>';
+	print_r($carros);
 	print '<br>';
+	foreach ($carros as $modelo => $detalhes) {
+		echo"=> modelo $modelo<br>\n";
+		foreach ($detalhes as $detalhe => $valor) {
+			echo "   - detalhe $detalhe => $valor<br>\n";
+		}
+	}
 	print '<br>';
+	array_unshift($cores, "preto");
+	array_push($cores, "lilás");
+	print_r($cores);
 	print '<br>';
 	print '<br>';
 	print '<br>';
