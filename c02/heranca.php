15a61f84c8b8ce1b780bef1ca4cff43441a4d75b
Jurandir <jura.webdeveloper@gmail.com>
2019-05-21 21:06:22 -0300

    polimorfismo e abstracao

--- c02/heranca.php
+++ c02/heranca.php
@@ -14,23 +14,8 @@ $contas[] =  new ContaCorrente(6677,'CC.1234.56',100,500);
 $contas[] =  new ContaPoupanca(6678,'PP.1234.57',100);
 $contas[] =  new ContaPoupanca(6678,'PP.2345.78',200);
 
-foreach($contas as $key => $conta) {
-    print "Conta: {$conta->getInfo()} <br>";
-    print "  Saldo atual: {$conta->getSaldo()} <br>";
-
-    $conta->depositar(200);
-
-    print "Depósito de 200.<br>";
-    print "  Saldo atual: {$conta->getSaldo()} <br>";
-
-    if ($conta->retirar(700)) {
-        print "Retirada de 700. <br>";
-    } else {
-        print "Retirada de 700 não permitida.<br>";
-    }
-    print "  Saldo atual: {$conta->getSaldo()} <br>";
-}
-print '<br>';
+echo '<pre>';
+print_r($contas);
 print '<br>';
 print '<br>';
 print '<br>';
