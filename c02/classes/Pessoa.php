fcf00301b574645235d0bb29dcb0cbfae30ca99f
Jurandir <jura.webdeveloper@gmail.com>
2019-05-21 22:25:35 -0300

    encapsulamento teste private2

--- c02/classes/Pessoa.php
+++ c02/classes/Pessoa.php
@@ -2,6 +2,23 @@
 class Pessoa {
     private $nome;
     private $endereco;
-    private $nascimento;    
+    private $nascimento;
+
+    public function __construct($nome, $endereco) {
+        $this->nome = $nome;
+        $this->endereco = $endereco;
+    }
+
+    public function setNascimento($nascimento) {
+        $partes = explode('-', $nascimento);
+        if(count($partes)==3) {
+            if(checkdate($partes[1], $partes[2], $partes[0])) {
+                $this->nascimento = $nascimento;
+                    return true;
+            }
+            return false;
+        }
+        return false;
+    }
 }
 ?>
--- c02/encapsulamento.php
+++ c02/encapsulamento.php
@@ -5,7 +5,7 @@
 <?php
 print '<h3>Public</h3>';
 require_once 'classes/Pessoa.php';
-
+/*
 $p1 = new Pessoa;
 
 $p1->nome = 'Maria Silva';
@@ -15,9 +15,23 @@ $p1->telefone = '(51) 1234-5678';
 
 echo '<pre';
 print_r($p1);
-
+*/
 print '<h3>Private</h3>';
+$p2 = new Pessoa('maria Silva', 'Rua Bento Goncalves');
+if($p2->setNascimento('01 de maio de 2015')) {
+    print "Attr 01 de maio <br>";
+} else {
+    print 'nao attr <br>';
+}
+if($p2->setNascimento('2015-12-30')) {
+    print 'Atribuiu 2015-12-30';
+} else {
+    print 'Não atribuiu';
+}
 
+
+echo '<pre';
+print_r($p2);
 
 print '<br>';
 print '<br>';
