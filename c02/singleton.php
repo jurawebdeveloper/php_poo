a4f7e996af101982ecadc7b14af0444008946869
jura <jura.webdeveloper@gmail.com>
2019-05-24 20:31:07 -0300

    singleton

--- c02/singleton.php
+++ c02/singleton.php
@@ -3,4 +3,12 @@ require_once 'classes/Preferencias.php';
 //obtem uma instância
 $p1 = Preferencias::getInstance();
 print 'Linguagem: '. $p1->getData('language'). "<br>";
+
+$p1->setData('language', 'pt');
+print 'A nova linguagem é: '. $p1->getData('language') ."<br>";
+
+$p2 = Preferencias::getInstance();
+print 'Linguagem: '. $p2->getData('language'). "<br>";
+//DESCOMENTAR PARA GRAVAR
+$p1->save();
 ?>
