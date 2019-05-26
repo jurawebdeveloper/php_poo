a92ceee12a1a11fe4592f67c72ce83e07c71ec2d
jura <jura.webdeveloper@gmail.com>
2019-05-26 11:42:58 -0300

    conexao pdo postgresql

---
+++ c02/pdo_insere.php
@@ -0,0 +1,21 @@
+<?php
+//conecta PostgreSql
+/*
+    try{
+        $conn = new PDO('pgsql:dbname=livro; user=postgres; password=1234; host=localhost');
+        $conn->exec("DELETE FROM famosos");
+        $conn->exec("INSERT INTO famosos VALUES (1, 'Érico Veríssimo')");
+        $conn->exec("INSERT INTO famosos VALUES (1, 'John Lenon')");
+        $conn->exec("INSERT INTO famosos VALUES (1, 'Mahatma Ghandi')");
+        $conn = null;
+    }
+    catch(PDOExeption $e) {
+        print "Erro!:"."\n";
+    }
+*/
+
+try{
+    
+}
+    
+?>
