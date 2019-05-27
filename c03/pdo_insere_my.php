f3290125c9be84a9252570954def741131e2f205
jura <jura.webdeveloper@gmail.com>
2019-05-27 13:12:04 -0300

    css

--- c03/pdo_insere_my.php
+++ c03/pdo_insere_my.php
@@ -2,7 +2,7 @@
 //conecta MySql
     try{
         $conn = new PDO('mysql: host=localhost; port=3306; dbname=livro','root','');
-        $conn->exec("CREATE TABLE famosos (codigo INT, nome VARCHAR(70))");
+        //$conn->exec("CREATE TABLE famosos (codigo INT, nome VARCHAR(70))");
         $conn->exec("DELETE FROM famosos");
         $conn->exec("INSERT INTO famosos VALUES (1, 'Airton Senna')");
         $conn->exec("INSERT INTO famosos VALUES (1, 'Anita Garibaldi')");
