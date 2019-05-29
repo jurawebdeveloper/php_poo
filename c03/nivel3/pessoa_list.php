2b212d94cb2c131f07ef812350b71417404c5478
jura <jura.webdeveloper@gmail.com>
2019-05-29 18:33:37 -0300

    pessoa list nao funciona

---
+++ c03/nivel3/pessoa_list.php
@@ -0,0 +1,24 @@
+<?php
+    $conn = mysqli_connect('127.0.0.1','root','','livro');
+    if(!empty($_GET['action']) AND $_GET['action']='delete'){
+        $id = (int) $_GET['id'];
+        $result = mysqli_query($conn,"DELETE FROM pessoa WHERE id = '{$id}'");
+    }
+    $result = mysqli_query($conn,"SELECT * FROM pessoa ORDER BY id");
+    $row = mysqli_fetch_assoc($result);
+    echo '<pre>'; print_r($row); exit;
+    $items = '';
+    while($row = mysqli_fetch_assoc($result)){
+        $item = file_get_contents('html/item.html');
+        $item = str_replace('{id}', $row['id'], $item);
+        $item = str_replace('{nome}', $row['nome'], $item);
+        $item = str_replace('{endereco}', $row['endereco'], $item);
+        $item = str_replace('{bairro}', $row['bairro'], $item);
+        $item = str_replace('{telefone}', $row['telefone'], $item);
+
+        $items = $item;
+    }
+    $list = file_get_contents('html/list.html');
+$list = str_replace('{items}', $items, $list);
+print $list;
+?>
