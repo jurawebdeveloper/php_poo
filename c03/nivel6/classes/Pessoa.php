e0a84b1a1715c2dbb4ec672e25db85ae810e3a78
jura <jura.webdeveloper@gmail.com>
2019-06-01 20:21:53 -0300

    nivel 6 pessoa nao funciona

--- c03/nivel6/classes/Pessoa.php
+++ c03/nivel6/classes/Pessoa.php
@@ -9,44 +9,46 @@ class Pessoa {
             $user = $conexao['user'];
             $pass = $conexao['pass'];
 
-            self::conn = new PDO('mysql: host = {$host}; dbname = {$name}',$user, $pass);
+            //self::conn = new PDO('mysql:host={$host};dbname={$name}',$user, $pass);
+            self::$conn = new PDO('mysql:host=localhost;dbname=livro',$user, $pass);
             //PDO('mysql:host=localhost;dbname=livro', $username, $password)
-            self::conn->setAttribute(PDO:::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         }
         return self::$conn;
     }
-    public function save($pessoa);
-    $conn = self::getConnection();
+        public function save($pessoa){
+        $conn = self::getConnection();
 
-    if(empty($pessoa['id'])){
-        $result = $conn->query("SELECT max(id) as last_id FROM pessoa");
-        $row = $result->fetch();
-        $pessoa['id'] = (int) $row['last_id'] + 1;
+        if(empty($pessoa['id'])){
+            $result = $conn->query("SELECT max(id) as last_id FROM pessoa");
+            $row = $result->fetch();
+            $pessoa['id'] = (int) $row['last_id'] + 1;
 
-        $sql = "INSERT INTO pessoa VALUES
-        (
-        :id, :nome, :endereco,
-        :bairro, :telefone,
-        :email, :id_cidade)";
-    }else{
-        $sql = "UPDATE pessoa SET
-        nome = :nome,
-        endereco = :endereco,
-        bairro = :bairro,
-        telefone = :telefone,
-        email = :email,
-        id_cidade = :id_cidade,
-        WHERE id = :id";
+            $sql = "INSERT INTO pessoa VALUES
+            (
+            :id, :nome, :endereco,
+            :bairro, :telefone,
+            :email, :id_cidade)";
+        }else{
+            $sql = "UPDATE pessoa SET
+            nome = :nome,
+            endereco = :endereco,
+            bairro = :bairro,
+            telefone = :telefone,
+            email = :email,
+            id_cidade = :id_cidade,
+            WHERE id = :id";
+        }
+        $result = $conn->prepare($sql);
+        $result->execute([
+            ':id'=>$pessoa['id'], 
+            ':nome'=>$pessoa['nome'], 
+            ':endereco'=>$pessoa['endereco'], 
+            ':bairro'=>$pessoa['bairro'], 
+            ':telefone'=>$pessoa['telefone'], 
+            ':email'=>$pessoa['email'], 
+            ':id_cidade'=>$pessoa['id_cidade'] 
+
+            ]);
     }
-    $result = $conn->prepare($sql);
-    $result->execute([
-        ':id'=>$pessoa['id'], 
-        ':nome'=>$pessoa['nome'], 
-        ':endereco'=>$pessoa['endereco'], 
-        ':bairro'=>$pessoa['bairro'], 
-        ':telefone'=>$pessoa['telefone'], 
-        ':email'=>$pessoa['email'], 
-        ':id_cidade'=>$pessoa['id_cidade'] 
-
-        ])
 }
