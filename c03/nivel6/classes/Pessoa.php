58a888364142b409d5150e6c3c41b49424b18d8d
jura <jura.webdeveloper@gmail.com>
2019-06-01 20:08:49 -0300

    nivel 6 pessoa

--- c03/nivel6/classes/Pessoa.php
+++ c03/nivel6/classes/Pessoa.php
@@ -1,62 +1,52 @@
 <?php
 class Pessoa {
-    
-    public static function save($pessoa) {
-        $username = 'root';
-        $password = '';
-        $conn = new PDO('mysql:host=localhost;dbname=livro', $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        if(empty($pessoa['id'])) {
-            $result = $conn->query("SELECT max(id) as last_id FROM pessoa");
-            $row = $result->fetch();
-            $pessoa['id'] = (int) $row['last_id'] +1;
-            $sql = "INSERT INTO pessoa (id, nome, endereco, bairro,
-            telefone, email, id_cidade)
-            VALUES ( '{$pessoa['id']}', '{$pessoa['nome']}', '{$pessoa['endereco']}',
-            '{$pessoa['bairro']}', '{$pessoa['telefone']}',
-            '{$pessoa['email']}', '{$pessoa['id_cidade']}'
-            )";  
-            //print_r($sql); exit;
-        }else {
-            $sql = "UPDATE pessoa SET nome = '{$pessoa['nome']}',
-            endereco = '{$pessoa['endereco']}',
-            bairro = '{$pessoa['bairro']}',
-            telefone = '{$pessoa['telefone']}',
-            email = '{$pessoa['email']}',
-            id_cidade = '{$pessoa['id_cidade']}'
-            WHERE id = '{$pessoa['id']}'";
+    private static $conn;
+    public static function getConnection(){
+        if(empty(self::conn)){
+            $conexao = parse_ini_file('config/livro.ini');
+            $host = $conexao['host'];
+            $name = $conexao['name'];
+            $user = $conexao['user'];
+            $pass = $conexao['pass'];
+
+            self::conn = new PDO('mysql: host = {$host}; dbname = {$name}',$user, $pass);
+            //PDO('mysql:host=localhost;dbname=livro', $username, $password)
+            self::conn->setAttribute(PDO:::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         }
-        return $conn->query($sql);
+        return self::$conn;
     }
-    public static function find($id) {
-        $username = 'root';
-        $password = '';
-        $conn = new PDO('mysql:host=localhost;dbname=livro', $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $result = $conn->query("SELECT * FROM pessoa WHERE id='{$id}'");
-        return $result->fetch();
+    public function save($pessoa);
+    $conn = self::getConnection();
+
+    if(empty($pessoa['id'])){
+        $result = $conn->query("SELECT max(id) as last_id FROM pessoa");
+        $row = $result->fetch();
+        $pessoa['id'] = (int) $row['last_id'] + 1;
+
+        $sql = "INSERT INTO pessoa VALUES
+        (
+        :id, :nome, :endereco,
+        :bairro, :telefone,
+        :email, :id_cidade)";
+    }else{
+        $sql = "UPDATE pessoa SET
+        nome = :nome,
+        endereco = :endereco,
+        bairro = :bairro,
+        telefone = :telefone,
+        email = :email,
+        id_cidade = :id_cidade,
+        WHERE id = :id";
     }
-    
-    public static function delete($id)
-    {
-        $username = 'root';
-        $password = '';
-        $conn = new PDO('mysql:host=localhost;dbname=livro', $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        $result = $conn->query("DELETE FROM pessoa WHERE id='{$id}'");
-        return $result;
-    }
-      
-    public static function all(){
-        $username = 'root';
-        $password = '';
-        $conn = new PDO('mysql:host=localhost;dbname=livro', $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        $result = $conn->query("SELECT * FROM pessoa ORDER BY id");
-        //return $result->fetchAll();
-        return $result->fetchAll();
-    }
+    $result = $conn->prepare($sql);
+    $result->execute([
+        ':id'=>$pessoa['id'], 
+        ':nome'=>$pessoa['nome'], 
+        ':endereco'=>$pessoa['endereco'], 
+        ':bairro'=>$pessoa['bairro'], 
+        ':telefone'=>$pessoa['telefone'], 
+        ':email'=>$pessoa['email'], 
+        ':id_cidade'=>$pessoa['id_cidade'] 
+
+        ])
 }
-?>
