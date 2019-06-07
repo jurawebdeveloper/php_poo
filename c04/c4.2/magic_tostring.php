445d43e45d07827257bdc320b7ff496c315aa657
jura <jura.webdeveloper@gmail.com>
2019-06-07 15:11:31 -0300

    to string ok

--- c04/c4.2/magic_tostring.php
+++ c04/c4.2/magic_tostring.php
@@ -1,10 +1,24 @@
 <?php 
-class Auth { //classe instanciada em outra classe  
-    protected static $_dbObject = null;  
-    public static function setDb($dbAdapter) {
-             self::$_dbObject = $dbAdapter;  
-    }  
-    public function authenticate ($username, $password) {
-                /* @stub		  verificar login utilizando o $this->getDb para retornar o objeto de abstração com o banco		*/	    
-    }  
-    protected function getDb () {   return self::$_dbObject;  }}
+class Titulo{
+    private $data;
+    
+    public function __set($propriedade,$valor){
+        $this->data[$propriedade] = $valor;
+    }
+
+    public function __get($propriedade){
+        return $this->$data[$propriedade];
+    }
+
+    public function __toString(){
+        return json_encode($this->data);
+    }
+
+}
+
+$titulo = new Titulo;
+$titulo->dt_vencimento = '2015-05-20';
+$titulo->valor = 12345;
+$titulo->juros = 0.1;
+$titulo->multa = 2;
+print 'O conteúdo do título é: ' .$titulo;
