ff4ec0100acc544bceb4f0c2d096e8e5eb7b58e5
jura <jura.webdeveloper@gmail.com>
2019-07-04 12:49:33 -0300

    file csv

--- c04/c4.4/spl_file_object.php
+++ c04/c4.4/spl_file_object.php
@@ -1,7 +1,8 @@
 <?php 
-$file = new SplFileInfo('index.html');
+$file = new SplFileObject('index.html');
 print 'Nome: '.$file->getFileName().'<br>'.PHP_EOL;
-print 'Extansão: '.$file->getExtension().'<br>'.PHP_EOL;
+print 'Extensão: '.$file->getExtension().'<br>'.PHP_EOL;
 
-$file2 = new SplFile
-
+$file2 = new SplFileObject("novo.txt", "w");
+$bytes = $file2->fwrite('Olá Mundo PHP' . PHP_EOL);
+print 'Bytes escritos ' . $bytes . PHP_EOL;
