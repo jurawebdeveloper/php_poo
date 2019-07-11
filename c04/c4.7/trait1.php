853c2f86f17bdc8dd60b9e15d91c6ede19a8d6f8
jura <jura.webdeveloper@gmail.com>
2019-07-11 13:25:29 -0300

    trait1

---
+++ c04/c4.7/trait1.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'classes/Record.php';
+class Pessoa extends Record{
+	const TABLENAME = 'pessoas';
+}
+
+class Fornecedor extends Record{
+	const TABLENAME = 'fornecedores';
+}
+
+class Produto extends Record{
+	const TABLENAME = 'produtos';
+}
+$p = new Pessoa;
+$p->load(1);
+print '<br>'.PHP_EOL;
+$p->nome='Maria da Silva';
+$p->endereco='Rua das Flores';
+$p->numero='123';
+$p->save();
+print '<br>'.PHP_EOL;
+$p->delete(3);
+print '<br>'.PHP_EOL;
