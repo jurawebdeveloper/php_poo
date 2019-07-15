721d0cce7d7d10fb6e0a32ba64476747c82c8536
jura <jura.webdeveloper@gmail.com>
2019-07-15 16:54:50 -0300

    namespaces

---
+++ c04/c4.10/namespace1.php
@@ -0,0 +1,14 @@
+<?php
+namespace Application;
+class Form{}
+namespace Components;
+class Form{}
+
+var_dump(new Form);
+print '<br>';
+var_dump(new \Components\Form);
+print '<br>';
+var_dump(new \Application\Form );
+print '<br>';
+var_dump(new \SplFileInfo('/etc/shadow'));
+print '<br>';
