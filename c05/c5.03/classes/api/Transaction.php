fc1eabb769958083c90c3c03fe22165fe22321ab
jura <jura.webdeveloper@gmail.com>
2019-09-10 15:25:40 -0300

    Transaction EXEMPLO

--- c05/c5.03/classes/api/Transaction.php
+++ c05/c5.03/classes/api/Transaction.php
@@ -1,7 +1,7 @@
 <?php
 final class Transaction{
 	private static $conn;
-	private static __construct() {}
+	private function __construct() {}
 
 	public static function open($database){
 		if(empty(self::$conn)){
---
+++ c05/c5.03/exemplo_transacao.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'classes/ar/Produto.php';
+require_once 'classes/api/Connection.php';
+require_once 'classes/api/Transaction.php';
+print 'Teste...';
+try{
+	Transaction::open('estoque');
+	$conn = Transaction::get();
+	Produto::setConnection($conn);
+
+	$p1 = new Produto;
+	$p1->descricao = 'Vinho Brasileiro';
+	$p1->estoque = 10;
+	$p1->preco_custo = 12;
+	$p1->preco_venda = 18;
+	$p1->codigo_barras = '13523453234234';
+	$p1->data_cadastro = date('Y-m-d');
+	$p1->origem = 'N';
+	$p1->save();
+
+	//throw new Exception('Exceção proposital');
+	$p2 = new Produto;
+	$p2->descricao = 'Vinho Importado';
+	$p2->estoque = 10;
+	$p2->preco_custo = 18;
+	$p2->preco_venda = 29;
+	$p2->codigo_barras = '73450345423423';
+	$p2->data_cadastro = date('Y-m-d');
+	$p2->origem = 'I';
+	$p2->save();
+	Transaction::close();
+}
+catch (Exception $e) {
+	Transaction::rollback();
+	print $e->getMessage();
+}
