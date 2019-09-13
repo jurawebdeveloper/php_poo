5b8d8f7d013b57e5ed57dbbf7f57df5b0b87bd7c
jura <jura.webdeveloper@gmail.com>
2019-09-13 16:59:05 -0300

    record toArray()

--- c05/c5.03/classes/api/Record.php
+++ c05/c5.03/classes/api/Record.php
@@ -1,5 +1,5 @@
 <?php
-abstract class Connection{
+abstract class Record{
 	protected $data;
 	public function __construct($id = NULL){
 		if($id){
@@ -16,5 +16,27 @@ abstract class Connection{
 		if(method_exists($this, 'set_'.$prop)){
 			call_user_func(array($this, 'set_'.$prop), $value);
 		}
+	}
+	public function __get($prop) {
+		if(method_exists($this, 'get_'.$prop)){
+			return call_user_func(array($this, 'get_'.$prop));
+		}else{
+			if (isset($this->data[$prop])){
+				return $this->data[$prop];
+			}
+		}
+	}
+	public function __isset($prop){
+		return isset($this->data[$prop]);
+	}
+	private function getEntity(){
+		$class = getClass($this);
+		return constant("{$class}::TABLENAME");
+	}
+	public function fromArray($data){
+		$this->data = $data;
+	}
+	public function toArray(){
+		return $this->data;
 	}
 }
