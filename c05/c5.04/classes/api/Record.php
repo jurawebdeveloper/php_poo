bd61c9a2bf2b51eb8601fd77af5db5c7d056a6cc
jura <jura.webdeveloper@gmail.com>
2019-09-20 19:45:04 -0300

    record.php __set

---
+++ c05/c5.04/classes/api/Record.php
@@ -0,0 +1,32 @@
+<?php
+abstract class Record{
+	protected $data;
+	public function __construct($id = NULL){
+		if($id){
+			$object = $this->load($id);
+			if($object){
+				$this->fromArray($object->toArray());
+			}
+		}
+	}
+	public function __clone(){
+		unset($this->data['id']);
+	}
+	public function __set($prop, $value){
+		if(method_exists($this, 'set_'.$prop)){
+			call_user_func(array($this, 'set_'.$prop),$value);
+		}else{
+			if ($value === NULL) {
+			unset($this->data[$prop]);
+			}else{
+				$this->data($prop) = $value;
+			}
+		}
+	}
+
+
+
+
+
+
+}
---
+++ c05/c5.04/record_novo.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'classes/api/Transaction.php';
+require_once 'classes/api/Connection.php';
+require_once 'classes/api/Logger.php';
+require_once 'classes/api/LoggerTXT.php';
+require_once 'classes/api/Record.php';
+require_once 'classes/model/Produto.php';
