b34f96579494a2801681aa300ff9634c6f78d2bb
jura <jura.webdeveloper@gmail.com>
2019-10-01 16:06:57 -0300

    rcord_protect

--- c05/c5.04/classes/model/Produto.php
+++ c05/c5.04/classes/model/Produto.php
@@ -1,4 +1,12 @@
 <?php
 class Produto extends Record{
 	const TABLENAME = 'produto';
+
+	public function set_estoque($estoque){
+		if(is_numeric($estoque) and $estoque > 0){
+			$this->data['estoque'] = $estoque;
+		}else{
+			throw new Exception("Estoque {$estoque} inválido em ".__CLASS__);
+		}
+	}
 }
