57fa8a710862b2b8c11da3971d3507b58d0ffe9c
jura <jura.webdeveloper@gmail.com>
2019-10-02 11:32:02 -0300

    function transform

--- c05/c5.05/classes/api/Criteria.php
+++ c05/c5.05/classes/api/Criteria.php
@@ -1,1 +1,34 @@
 <?php
+class Criteria{
+	private $filters;
+	function __construct(){
+		$this->filters = array();
+	}
+	public function add($variable, $compare_operator, $value, $logic_operator = 'and'){
+		if(empty($this->filters)){
+			$logic_operator = NULL;
+		}
+		$this->filters[] = [$variable, $compare_operator, $this->transform($value), $logic_operator];
+	}
+	public function transform($value){
+		if(is_array($value)){
+			foreach ($value as $x) {
+				if(is_integer($x)){
+					$foo[] = $x;
+				}elseif (is_string($x)) {
+					$foo[] = "'$x'";
+				}
+			}
+			$result = '('.implode(',', $foo).')';
+		}elseif (is_string($value)) {
+			$result = "'$value'";
+		}elseif (is_null($value)) {
+			$result = 'NULL';
+		}elseif (is_bool($value)) {
+			$result = $value ? 'TRUE' ; 'FALSE';
+		}else{
+			$result = $value;
+		}
+		return $result;
+	}
+}
