9000199286ffb10f992a1c023ee3de3a43312fdb
jura <jura.webdeveloper@gmail.com>
2019-10-14 15:27:20 -0300

    collectio_get inicio

---
+++ c05/c5.06/collection_get.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'classes/api/Transaction.php';
+require_once 'classes/api/Connection.php';
+require_once 'classes/api/Criteria.php';
+require_once 'classes/api/Repository.php';
+require_once 'classes/api/Record.php';
+require_once 'classes/api/Logger.php';
+require_once 'classes/api/LoggerTXT.php';
+require_once 'classes/model/Produto.php';
+print 'Teste...';
+
